3ce34e938bd51efab43bf182ca4fbcef063b7fd9
FzzY#2284 <99145322+FzzyYT69@users.noreply.github.com>
2022-08-05 03:37:18 +0500

    Update main.lua

--- qb-business_senators/client/main.lua
+++ qb-business_senators/client/main.lua
@@ -2,14 +2,44 @@ local QBCore = exports["qb-core"]:GetCoreObject()
 
 
 RegisterNetEvent("Open:GiveJob:Manu", function()
-    local keyboard, id, job, grade = exports["nh-keyboard"]:Keyboard({
-        header = "Business Senators (burgershot, catcafe, pizza, beanmachine)", 
-        rows = {"Id", "Job", "Grade"}
-    })
-
-    if keyboard then
-        if id and job and grade then
-            TriggerServerEvent("Give:Business:Job", id, job, grade)
+    if Config.input == 'nh-keyboard' then
+        local keyboard, id, job, grade = exports["nh-keyboard"]:Keyboard({
+            header = "Business Senators (burgershot, catcafe, pizza, beanmachine)", 
+            rows = {"Id", "Job", "Grade"}
+        })
+    
+        if keyboard then
+            if id and job and grade then
+                TriggerServerEvent("Give:Business:Job", id, job, grade)
+            end
+        end
+    elseif Config.input == 'qb-input' then
+        local dialog = exports['qb-input']:ShowInput({
+            header = "Business Senators (burgershot, catcafe, pizza, beanmachine",
+            --submitText = ,
+            inputs = {
+                {
+                    type = 'number',
+                    isRequired = true,
+                    name = 'id',
+                    text = "ID OF PLAYER"
+                },
+                {
+                    type = 'text',
+                    isRequired = true,
+                    name = 'job',
+                    text = "Business Name"
+                },
+                {
+                    type = 'number',
+                    isRequired = true,
+                    name = 'grade',
+                    text = "Business Grade"
+                },
+            }
+        })
+        if dialog then
+            TriggerServerEvent("Give:Business:Job", dialog.id, dialog.job, dialog.grade)
         end
     end
 
@@ -27,6 +57,7 @@ exports['qb-target']:AddCircleZone("senator-menu", vector3(-551.19, -201.74, 38.
         { -- This is the first table with options, you can make as many options inside the options table as you want
             type = "client", -- This specifies the type of event the target has to trigger on click, this can be "client", "server", "command" or "qbcommand", this is OPTIONAL and will only work if the event is also specified
             event = "Open:GiveJob:Manu", -- This is the event it will trigger on click, this can be a client event, server event, command or qbcore registered command, NOTICE: Normal command can't have arguments passed through, QBCore registered ones can have arguments passed through
+            icon = 'fas fa-example', -- This is the icon that will display next to this trigger option
             label = 'Open Senator Menu', -- This is the label of this option which you would be able to click on to trigger everything, this has to be a string
             targeticon = 'fa-solid fa-chess-king', -- This is the icon of the target itself, the icon changes to this when it turns blue on this specific option, this is OPTIONAL
         },
